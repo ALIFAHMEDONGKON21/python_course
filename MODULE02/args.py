5b5167d0a0f170a947471e7ced376fa2b14c97ba
MD ALIF <96830817+ALIFAHMEDONGKON21@users.noreply.github.com>
2024-01-24 14:27:45 +0600

    Add files via upload

---
+++ MODULE02/args.py
@@ -0,0 +1,5 @@
+def lot_of_do(num1,num2,*args):
+    print(args)
+
+
+total=lot_of_do(10,20,30,50,70)
---
+++ MODULE02/function.py
@@ -0,0 +1,12 @@
+# def doble_it(num):
+#     res=num*2
+#     print(num)
+#     return res
+# doble_it(20)
+
+def sum(num1, num2):
+    res=num1+num2
+    return res
+
+total=sum(10,10)
+print(total)
